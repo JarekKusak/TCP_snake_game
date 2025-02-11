4c26126bd4e561d2f43bcfd1a835e6dd7793a84e
Jaroslav Kusák <jarekusak@seznam.cz>
2025-02-11 10:57:53 +0100

    developer docs

---
+++ doc/developer-docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"tcpsnake","l":"Client"},{"p":"tcpsnake","l":"Common"},{"p":"tcpsnake","l":"Position"},{"p":"tcpsnake","l":"Server"}];updateSearchResults();
